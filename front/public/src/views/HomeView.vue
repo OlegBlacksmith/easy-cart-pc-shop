1161f9b444092fecb07dbba39bf3e75bb60689c5
Oleg Blacksmith <OKuznetsov21041992@outlook.com>
2024-10-28 23:41:17 +0400

    fix: namespace change

---
+++ front/public/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+public class Product { public string PublicId { get; set; } public string Name {
+get; set; } public int Stock { get; set; } // Поле для хранения динамических
+характеристик товара public string DetailsJson { get; set; } public int
+CategoryId { get; set; } public Category Category { get; set; } public
+ManufacturerId { get; set; } }
